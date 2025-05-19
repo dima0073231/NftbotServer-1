a79e7c25a7c24d51badb715a27ebf6295e92e074
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-05-19 22:43:56 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
 connectDB();
@@ -34,7 +33,7 @@ app.get("/api/users", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/api/users", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
